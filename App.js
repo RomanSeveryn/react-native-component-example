820ef1a0ab04e40b9a64a9c20d46dbfb9835e51c
Roman Severyn <romaseverin50@gmail.com>
2021-11-17 22:22:23 +0200

    example FlatList

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, Linking, ScrollView, RefreshControl } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking, ScrollView, RefreshControl, FlatList } from 'react-native';
 
 export default function App() {
 
   const [refrech, setRefresh] = useState(false)
 
   const [state, setState] = useState([
-    {key: 1, number: '1'},
-    {key: 2, number: '2'},
-    {key: 3, number: '3'},
-    {key: 4, number: '4'},
-    {key: 5, number: '5'},
-    {key: 6, number: '6'},
-    {key: 7, number: '7'},
-    {key: 8, number: '8'},
-    {key: 9, number: '9'},
+    {key: '1', number: '1'},
+    {key: '2', number: '2'},
+    {key: '3', number: '3'},
+    {key: '4', number: '4'},
+    {key: '5', number: '5'},
+    {key: '6', number: '6'},
+    {key: '7', number: '7'},
+    {key: '8', number: '8'},
+    {key: '9', number: '9'},
 
   ])
   const onRefresh = () => {
@@ -24,20 +24,32 @@ export default function App() {
     setRefresh(false)
   }
   return (
-    <View style={styles.container}>
-      <ScrollView refreshControl={
-        <RefreshControl refreshing={refrech} onRefresh={onRefresh}/>
-      }>
-      {
-        state.map((item) => {
-          return (
+    <View>
+      <FlatList data={state} renderItem={({item}) => 
+          (
       <View style={styles.item}>
         <Text style={styles.text} key={item.key}>{item.number}</Text>
       </View>
-        )})
-      }
-      </ScrollView>
+          )} 
+          refreshControl={
+                <RefreshControl refreshing={refrech} onRefresh={onRefresh}/>
+              }
+          />
     </View>
+    // <View style={styles.container}>
+    //   <ScrollView refreshControl={
+    //     <RefreshControl refreshing={refrech} onRefresh={onRefresh}/>
+    //   }>
+    //   {
+    //     state.map((item) => {
+    //       return (
+    //   <View style={styles.item}>
+    //     <Text style={styles.text} key={item.key}>{item.number}</Text>
+    //   </View>
+    //     )})
+    //   }
+    //   </ScrollView>
+    // </View>
   );
 }
 
