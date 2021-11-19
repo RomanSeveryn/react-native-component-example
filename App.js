9a1a4014661c4dbb33666b876e80e997cb47785b
Roman Severyn <romaseverin50@gmail.com>
2021-11-19 12:39:40 +0200

    add navigation

--- App.js
+++ App.js
@@ -1,127 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, Linking, ScrollView, RefreshControl, FlatList, SectionList, TextInput, TouchableOpacity, TouchableHighlight, Pressable, Alert, ToastAndroid, Modal, Image } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
 
-  const [name, setName] = useState('')
-  const [submitted, setSubmitted] = useState(false)
-  const [showWarning, setShowWarning] = useState(false)
-
+function ScreenA({navigation}) {
 
   const onPressHandler = () => {
-    if (name.length > 3) {
-    setSubmitted(!submitted)
-    } else {
-    setShowWarning(true)
-    }
+    navigation.navigate('Screen_B')
   }
-
- 
   return (
-    <View style={styles.container}>
-      <Modal
-       visible={showWarning}
-       onRequestClose={() => setShowWarning(false)}
-       transparent={true}
-       >
-         <View style={styles.centered_view}>
-           <View style={styles.warning_modal}>
-           <View style={styles.warning_title}>
-             <Text style={styles.text}>
-               WARNING
-             </Text>
-           </View>
-                <Text>The text must have longer than 3 characters</Text>
-                <Pressable style={styles.warning_button} onPress={() => setShowWarning(false)}>
-                  <Text>
-                    OK
-                  </Text>
-                </Pressable>
-                
-           </View>
-         </View>
-      </Modal>
+    <View style={styles.body}>
       <Text style={styles.text}>
-        Please write your name:
+        Screen A
       </Text>
-      <TextInput style={styles.input} placeholder='name' onChangeText={(value) => setName(value)}/>
-      {/* <Button title={submitted ? 'Clear' : 'Submit'} onPress={onPressHandler} /> */}
-      <Pressable onPress={onPressHandler} hitSlop={{top: 10, bottom: 10, right: 10, left: 10}} style={({pressed}) => [styles.button, {
-    backgroundColor: pressed ? 'green' : 'blue'}]}>
+      <Pressable
+       style={({pressed}) => ({backgroundColor: pressed ? '#ddd' : '#0f0'})}
+       onPress={onPressHandler}
+       >
         <Text style={styles.text}>
-        {submitted ? 'Clear' : 'Submit'}
+          Go to Screen B
         </Text>
       </Pressable>
-      {submitted ?
-      <View>
-        <Text>
-        Your name is: {name}
-      </Text>
-      </View>
-      
-      
-      :
-      <Image style={styles.image} source={require('./assets/audi.png')}/>
-      // <Image style={styles.image} resizeMode='stretch' source={{uri: 'https://carta.com/wp-content/uploads/2019/08/axios.png'}}/>
-      }
     </View>
-  );
+  )
+}
+
+function ScreenB({navigation}) {
+
+  const onPressHandler = () => {
+    navigation.navigate('Screen_A')
+  }
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>
+        Screen B
+      </Text>
+      <Pressable
+       style={({pressed}) => ({backgroundColor: pressed ? '#ddd' : '#0f0'})}
+       onPress={onPressHandler}
+       >
+        <Text style={styles.text}>
+          Go to Screen A
+        </Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+      // screenOptions={{header: () => null}}
+      >
+        <Stack.Screen
+        name="Screen_A"
+        component={ScreenA}
+        // options={{
+        //   header: () => null
+        //   }}
+          />
+        <Stack.Screen
+        name="Screen_B"
+        component={ScreenB}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    justifyContent: 'center',
     alignItems: 'center'
   },
-  item: {
-    backgroundColor: 'violet',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 5,
-  },
-  text: {
-    color: 'black',
-    fontSize: 40,
-  },
-  input: {
-    padding: 10,
-    borderColor: 'grey',
-    borderWidth: 2,
-    fontSize: 14
-  },
-  button: {
-    marginTop: 30
-  },
-  centered_view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000099'
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#ffffff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20
-  },
-  warning_title: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff0',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20
-  },
-  warning_button: {
-    alignItems: 'center',
-    marginTop: 130
-  },
-  image: {
-    width: 150,
-    height: 150
-  }
-});
+   text: {
+     fontSize: 40,
+     fontWeight: 'bold',
+     margin: 10
+   }
+})
