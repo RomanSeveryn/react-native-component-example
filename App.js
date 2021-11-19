0d5f1c82f1f78ec988b4e64d4b4c339d452c152d
Roman Severyn <romaseverin50@gmail.com>
2021-11-19 16:16:57 +0200

    tab bottom navigaition with icons

--- App.js
+++ App.js
@@ -2,57 +2,18 @@ import React, {useState} from 'react';
 import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import ScreenA from './ScreenA';
+import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-
-
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-   text: {
-     fontSize: 40,
-     fontWeight: 'bold',
-     margin: 10
-   }
-})
-
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 
 export default function App() {
-  const [state, setState] = useState(1)
-  const awesomeFunc = () => {
-    setState((prev) => prev + 1)
-  }
-  function ScreenA({navigation}) {
-  
-    const onPressHandler = () => {
-      navigation.navigate('Screen_B')
-    }
-    return (
-      <View style={styles.body}>
-        <Text style={styles.text}>
-          Screen A
-        </Text>
-        <Pressable
-         style={({pressed}) => ({backgroundColor: pressed ? '#ddd' : '#0f0'})}
-         onPress={onPressHandler}
-         >
-          <Text style={styles.text}>
-            Go to Screen B
-          </Text>
-        </Pressable>
-        <Button title='Button' onPress={awesomeFunc}>My Button</Button>
-  
-      </View>
-    )
-  }
+
   
   return (
     <NavigationContainer>
@@ -61,25 +22,24 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
+            if (route.name === 'Screen_A') {
+              iconName = 'car';
+              size = focused ? 25 : 30;
+            } else if (route.name === 'Screen_B') {
+              iconName = 'settings';
+              size = focused ? 25 : 30;
             }
 
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
         })}
+        barStyle={{backgroundColor: '#694fad'}}
       >
         <Tab.Screen
         name="Screen_A"
         component={ScreenA}
-        options={{tabBarBadge: state}}
+        options={{tabBarBadge: 3}}
           />
         <Tab.Screen
         name="Screen_B"
